e0d6248bc2e3032b9495a7d15ea15e45f84132ec
naing <nainguyen3239@gmail.com>
2017-04-21 01:44:36 +0700

    Uploand lan 1 teamplate va db

---
+++ SHOPONLINE/SHOPONLINE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SHOPONLINE.Startup))]
+namespace SHOPONLINE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

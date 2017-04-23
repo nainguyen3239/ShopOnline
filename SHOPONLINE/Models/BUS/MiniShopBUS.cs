051aca492605fd9ea127244361f122f698c5b714
NguyenVanHien <nainguyen3239@gmail.com>
2017-04-23 18:49:53 +0700

    them trang chi tiet san pham va mot vai san pham

---
+++ SHOPONLINE/Models/BUS/MiniShopBUS.cs
@@ -0,0 +1,22 @@
+﻿using MiniShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SHOPONLINE.Models.BUS
+{
+    public class MiniShopBUS
+    {
+        public static IEnumerable<SANPHAM> DanhSach()
+            {
+                var db = new MiniShopConnectionDB();
+                return db.Query<SANPHAM>("select * from SANPHAM where TinhTrang = 0");
+            }
+        public static SANPHAM ChiTiet(String a)
+        {
+            var db = new MiniShopConnectionDB();
+            return db.SingleOrDefault<SANPHAM>("select * from SANPHAM where MaSanPham = @0",a);
+        }
+    }
+}
